6a6f0a17c7645bab03cf9952edaf877e28803aa9
Ivan5 <converse_ivan@hotmail.com>
2018-08-01 16:36:47 -0500

    Add some Exceptions

--- server/app/Services/AuthorizationService.js
+++ server/app/Services/AuthorizationService.js
@@ -1,7 +1,15 @@
+const InvalidAccessException = use('App/Exceptions/InvalidAccessException')
+const ResourceNotExistException = use('App/Exception/ResourceNotExistException')
+
 class AuthorizationService {
   verifyPermission(resource, user) {
+    if(!resource){
+      throw new ResourceNotExistException();
+    }
     if(resource.user-id != user.id){
       throw new Error();
     }
   }
 } 
+
+module.exports = new AuthorizationService();
